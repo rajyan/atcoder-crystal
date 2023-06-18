27e75593171e98dfa1d1ccd01e8687bb43844eb2
Yohta Kimura <kitakita7617@gmail.com>
2023-06-19 08:26:47 +0900

    fix test directory searching

--- add_test.bash
+++ add_test.bash
@@ -1,12 +1,12 @@
 #!/bin/bash -eux
 
-latest_file=$(find "$PWD/test" "$PWD" -name 'handmade-*.in' | sort -V | tail -n1)
-file_dir=${latest_file%/*}
-index=$(echo "$latest_file" | sed -E 's/^.*handmade-([0-9]+)\.in/\1/g')
+test_dir=$(find "$PWD/test" "$PWD" -name '*.in' | head -1 | xargs dirname)
+latest_file=$(find "$test_dir" -name 'handmade-*.in' | sort -V | tail -n1)
+index=$(echo "$latest_file" | sed -E 's/^.*-([0-9]+)\.in/\1/g')
 next_index=$(( ${index:-0} + 1 ))
 
-out_file="$file_dir/handmade-${next_index}.out"
-in_file="$file_dir/handmade-${next_index}.in"
+out_file="$test_dir/handmade-$next_index.out"
+in_file="$test_dir/handmade-$next_index.in"
 
 touch "$out_file" "$in_file"
 rmine "$out_file" "$in_file"
