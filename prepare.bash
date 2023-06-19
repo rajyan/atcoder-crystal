4dd2b47daa819b55cff90f07cae287f39f0c4d67
Yohta Kimura <kitakita7617@gmail.com>
2023-06-19 09:01:17 +0900

    move dump and assert to live templates

--- prepare.bash
+++ prepare.bash
@@ -7,13 +7,6 @@ file="$dir.cr"
 mkdir "$dir"
 pushd "$dir"
 touch "$file"
-cat <<EOF >> "$file"
-macro dump(*vs)
-  o="{% for v in vs %}{{v.id}}=#{{{v.id}}}\n{% end %}"
-  STDERR.puts o
-end
-
-EOF
 rmine "$PWD/$file"
 /home/linuxbrew/.linuxbrew/bin/oj d "$url"
 popd
