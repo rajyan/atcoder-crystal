6ac2c73afaa07425c66bd355dc528b6603ebc7c5
Yohta Kimura <kitakita7617@gmail.com>
2023-06-20 22:33:12 +0900

    refactor abc301 a,b

--- abc301_a/abc301_a.cr
+++ abc301_a/abc301_a.cr
@@ -1,15 +1,8 @@
 n = read_line.to_i
 s = read_line
 
-t = 0; a = 0
-n.times do |i|
-  if s[i] == 'T'
-    t += 1
-  else
-    a += 1
-  end
-end
-
+t = s.count('T')
+a = n - t
 if t == a
   puts s[-1] == 'T' ? 'A' : 'T'
 elsif t > a
--- abc301_b/abc301_b.cr
+++ abc301_b/abc301_b.cr
@@ -1,11 +1,12 @@
 n = read_line.to_i
 a = read_line.split.map(&.to_i)
+a << a[-1] + 1
 
-n.pred.times do |i|
+ans = n.times.map do |i|
   if a[i] < a[i + 1]
-    puts (a[i]...a[i + 1]).join(' ')
+    a[i]...a[i + 1]
   else
-    puts (-a[i]...-a[i + 1]).map(&.-).join(' ')
+    (-a[i]...-a[i + 1]).map(&.-)
   end
 end
-puts a[-1]
+puts ans.map(&.to_a).flatten.join(' ')
