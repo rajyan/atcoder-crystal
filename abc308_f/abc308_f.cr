09dacb2211aa8b5786aa605f254ec3b5a451c9b2
Yohta Kimura <kitakita7617@gmail.com>
2023-07-09 15:52:13 +0900

    refactor

--- abc308_f/abc308_f.cr
+++ abc308_f/abc308_f.cr
@@ -3,30 +3,29 @@ p = read_line.split.map(&.to_i).sort
 l = read_line.split.map(&.to_i)
 d = read_line.split.map(&.to_i)
 
-tree = RBST(Int32).new(p)
+tree = RBST(Int32, Void).new(p)
 ans = p.map(&.to_i64).sum
 ld = l.zip(d).sort_by { |(l, d)| -d }
 ld.each do |(l, d)|
   min = tree >= l
   next unless min
-  tree.delete(min)
+  tree.delete(min.key)
   ans -= d
 end
 
 puts ans
 
-class RBST(T)
-  @root : Node(T) | Nil = nil
+class RBST(T, K)
+  @root : Node(T, K)? = nil
   getter :root
   @comp : (T, T) -> Bool = ->(a : T, b : T) { a > b }
 
-  def initialize(a : Array(T) = [] of T)
+  def initialize(a : Array(T) = [] of T, &@comp : (T, T) -> Bool)
     a.each { |e| insert(e) }
   end
 
-  def initialize(a : Array(T) = [] of T, &block)
+  def initialize(a : Array(T) = [] of T, @comp = ->(a : T, b : T) { a > b })
     a.each { |e| insert(e) }
-    @comp = block
   end
 
   def size
@@ -41,11 +40,11 @@ class RBST(T)
     @root = insert(v, @root)
   end
 
-  def insert(v : T, node : Node(T) | Nil) : Node(T)
-    return Node(T).new(v) unless node
+  def insert(v : T, node : Node(T, K)?) : Node(T, K)
+    return Node(T, K).new(v) unless node
 
     left, right = split(node, rank(v))
-    merge(merge(left, Node(T).new(v)), right).not_nil!
+    merge(merge(left, Node(T, K).new(v)), right).not_nil!
   end
 
   def <<(v : T)
@@ -56,62 +55,39 @@ class RBST(T)
     @root = delete(v, @root)
   end
 
-  def delete(v : T, node : Node(T) | Nil) : Node(T) | Nil
+  def delete(v : T, node : Node(T, K)?) : Node(T, K)?
     return nil unless node
 
-    if @comp.call(node.key, v)
-      node.left = delete(v, node.left)
-    elsif @comp.call(v, node.key)
-      node.right = delete(v, node.right)
-    else
-      return merge(node.left, node.right)
-    end
-    node.fix
+    left, mid = split(node, rank(v))
+    w, right = split(mid, 1)
+    return nil if w.try(&.key) != v
+    merge(left, right)
   end
 
   def clear
     @root = nil
   end
 
-  def search(v : T, node : Node(T) = @root) : Node(T) | Nil
-    until node.nil?
-      node = if @comp.call(node.key, v)
-               node.left
-             elsif @comp.call(v, node.key)
-               node.right
-             else
-               return node
-             end
-    end
+  def search(v : T, node : Pointer(Node(T, K)?) = pointerof(@root)) : Node(T, K)?
+    n = nth(rank(v), node)
+    n.try(&.key) == v ? n : nil
   end
 
-  def lower_than(v : T, node : Node(T) = @root) : T | Nil
-    ret = nil
-    until node.nil?
-      node = if @comp.call(node.key, v)
-               node.left
-             else
-               ret = node.key
-               node.right
-             end
-    end
+  def lower_than(v : T, node : Pointer(Node(T, K)?) = pointerof(@root)) : Node(T, K)?
+    left, right = split(node.value, rank(v))
+    ret = left.try(&.last)
+    node.value = merge(left, right) # restore
     ret
   end
 
-  def <=(v : T)
+  def <(v : T)
     lower_than(v)
   end
 
-  def higher_than(v : T, node : Node(T) = @root) : T | Nil
-    ret = nil
-    until node.nil?
-      node = if @comp.call(v, node.key)
-               node.right
-             else
-               ret = node.key
-               node.left
-             end
-    end
+  def higher_than(v : T, node : Pointer(Node(T, K)?) = pointerof(@root)) : Node(T, K)?
+    left, right = split(node.value, rank(v))
+    ret = right.try(&.first)
+    node.value = merge(left, right) # restore
     ret
   end
 
@@ -119,35 +95,30 @@ class RBST(T)
     higher_than(v)
   end
 
-  def rank(v : T, node : Node(T) = @root) : Int32
+  def rank(v : T, node : Node(T, K) = @root) : Int32
     idx = 0
     until node.nil?
-      node = if v <= node.key
-               node.left
-             else
+      node = if @comp.call(v, node.key)
                idx += (node.left.try(&.size) || 0) + 1
                node.right
+             else
+               node.left
              end
     end
     idx
   end
 
-  def nth(n : Int32, node : Node(T) = @root) : Node(T) | Nil
-    until node.nil?
-      idx = node.left.try(&.size) || 0
-      return node if idx == n
-      node = if idx > n
-               node.left
-             else
-               n -= idx + 1
-               node.right
-             end
-    end
+  def nth(n : Int32, node : Pointer(Node(T, K)?) = pointerof(@root)) : Node(T, K)?
+    range(n, n + 1, node)
   end
 
-  # [l...r]
-  def range(l : Int32, r : Int32, node : Node(T) | Nil = @root)
-    split(split(node.dup, r)[0], l)[1]
+  def range(l : Int32, r : Int32, node : Pointer(Node(T, K)?) = pointerof(@root))
+    return nil unless node
+    mid, right = split(node.value, r)
+    left, mid = split(mid, l)
+    ret = mid.dup
+    node.value = merge(merge(left, mid), right).not_nil! # restore
+    ret
   end
 
   def [](n : Int32)
@@ -159,23 +130,23 @@ class RBST(T)
   end
 
   # {[0...k], [k...n]}
-  def split(node : Node(T) | Nil, k : Int32) : {Node(T) | Nil, Node(T) | Nil}
-    raise IndexError.new unless 0 <= k <= (node.try &.size || 0)
+  def split(node : Node(T, K)?, k : Int32) : {Node(T, K)?, Node(T, K)?}
     return {nil, nil} unless node
+    raise IndexError.new("k: #{k} is out of tree range") unless 0 <= k <= node.size
 
     sz = node.left.try(&.size) || 0
     if k <= sz
-      left, right = split(node.left.dup, k)
+      left, right = split(node.left, k)
       node.left = right
       {left, node.fix}
     else
-      left, right = split(node.right.dup, k - sz - 1)
+      left, right = split(node.right, k - sz - 1)
       node.right = left
       {node.fix, right}
     end
   end
 
-  def merge(left : Node(T) | Nil, right : Node(T) | Nil) : Node(T) | Nil
+  def merge(left : Node(T, K)?, right : Node(T, K)?) : Node(T, K)?
     return right unless left
     return left unless right
 
@@ -187,38 +158,37 @@ class RBST(T)
       right.fix
     end
   end
-
-  private def rotate_right(node : Node(T)) : Node(T)
-    top = node.left.not_nil!
-    top.right, node.left = node, top.right
-    node.fix
-    top.fix
-  end
-
-  private def rotate_left(node : Node(T)) : Node(T)
-    top = node.right.not_nil!
-    top.left, node.right = node, top.left
-    node.fix
-    top.fix
-  end
 end
 
-class Node(T) < Reference
-  @left : Node(T) | Nil
-  @right : Node(T) | Nil
-  property :left, :right
+class Node(T, K)
+  @left : Node(T, K)?
+  @right : Node(T, K)?
+  @val : K?; @acc : K?; @lazy : K?
+  property :left, :right, :val, :acc, :lazy
   getter :key, :size, :height
 
-  def initialize(@key : T)
-    @left = nil
-    @right = nil
-    @size = 1
-    @height = 1
+  def initialize(@key : T, @size : Int32 = 1, @height : Int32 = 1, @val = nil, @acc = nil, @lazy = nil); end
+
+  def propagate
   end
 
   def fix
     @size = (@left.try(&.size) || 0) + (@right.try(&.size) || 0) + 1
     @height = {@left.try(&.height) || 0, @right.try(&.height) || 0}.max + 1
     self
+  end
+
+  def dup
+    n = Node(T, K).new(key, size, height)
+    n.left, n.right = left.dup, right.dup
+    n
+  end
+
+  def first
+    left.try(&.first) || self
+  end
+
+  def last
+    right.try(&.last) || self
   end
 end
